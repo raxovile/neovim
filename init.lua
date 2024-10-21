e95a8d393ddc26b5515a96deb2b06396ed8900a2
Raphael Brand <rbr@objektkultur.de>
2024-10-21 22:21:58 +0200

    add basic configuration

---
+++ init.lua
@@ -0,0 +1,12 @@
+-- load global vim configurations
+require 'globals'
+
+-- load vim option configurations
+require 'options'
+
+-- load vim basic autocommands
+require 'basicAutocommands'
+
+-- load lazy.vim as Plugin Manager
+require 'lazyInstallation'
+
---
+++ lua/globals.lua
@@ -0,0 +1,14 @@
+-- Set golbal vim configuration
+--
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+vim.g.copilot_filetypes = {
+  ['*'] = true,
+}
