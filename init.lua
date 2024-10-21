e87498d1b91107387538d5c44f8655c61ed38bb4
Raphael Brand <rbr@objektkultur.de>
2024-10-21 22:25:04 +0200

    add git Ignore file into project

--- init.lua
+++ init.lua
@@ -10,3 +10,24 @@ require 'basicAutocommands'
 -- load lazy.vim as Plugin Manager
 require 'lazyInstallation'
 
+require('lazy').setup({
+  { 'tpope/vim-sleuth' },
+  { 'folke/lazy.nvim' },
+  { 'BurntSushi/ripgrep' },
+  { 'nvim-lua/plenary.nvim' },
+  { 'PProvost/vim-ps1' },
+  { 'nvim-lua/lsp-status.nvim' },
+  { 'rcarriga/nvim-dap-ui' },
+  { 'tpope/vim-fugitive' },
+  { 'KaiWalter/azure-functions.nvim' },
+  { 'dense-analysis/ale' },
+  { 'TheLeoP/powershell.nvim' },
+    { 
+    'olimorris/onedarkpro.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      vim.cmd.colorscheme 'onedark'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  })
